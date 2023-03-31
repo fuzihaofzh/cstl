9217fd5801499ab558a2c1e5c66150fd10c6d665
fuzihaofzh <fllubo@qq.com>
2023-03-31 08:07:48 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ python generate_swig.py
 cd cstl
 PYINCLUDE=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 swig -c++ -python -py3 cstl.i
+echo "Finish swig."
 g++ -std=c++11 -fPIC -c -x c++ cstl_wrap.cxx -o cstl_wrap.o -I$PYINCLUDE -O3
+echo "Finish g++ compile."
 g++ -shared cstl_wrap.o -o _cstl.so -O3
 cd ..
 python post_process.py
